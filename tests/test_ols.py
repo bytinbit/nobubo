2e5d263e8ae1d49c876ba3264801280bb849a33d
bytinbit <meline.sieber@hsr.ch>
2019-11-25 12:43:24 +0100

    setup tests for ols

---
+++ tests/test_ols.py
@@ -0,0 +1,46 @@
+import pytest
+import PyPDF2
+
+from nobubo import utils
+
+
+class TestMainMethods:
+
+    def test_assemble_to_collage():
+        pass
+
+    def test_create_output_files():
+        pass
+
+
+class TestHelperMethods:
+
+    def test_calculate_colsrows_left(self):
+        pass
+
+    def test_calculate_lowerleft_point(self):
+        pass
+
+    def test_calculate_upperright_point_rowsleft_below_0(self):
+        pass
+
+    def test_calculate_upperright_point_rowsleft_above_or_equal_0(self):
+        pass
+
+    def test_calculate_upperright_point_colsleft_above_0(self):
+        pass
+
+    def test_calculate_upperright_point_colsleft_equal_0(self):
+        pass
+
+    def test_calculate_upperright_point_colsleft_below_0(self):
+        pass
+
+    def test_adjust_factors(self):
+        pass
+
+    def test_advance_horizontally(self):
+        pass
+
+    def test_advance_vertically(self):
+        pass
