c091189a21e3bc89133544f3093bc8b87b6f5a07
bytinbit <meline.sieber@ost.ch>
2021-01-11 15:23:45 +0100

    update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nobubo-bytinbit",
-    version="1.1.0",
+    version="1.2.0",
     description="Nobubo assembles a digital pdf sewing pattern and chops it into a desired output size to be printed.",
     long_description=long_description,
     long_description_content_type="text/markdown",    
